ce5823bf0bef0ecd0809696042dfedf68727325c
Giovanni Janial <giovannijanial@gmail.com>
2022-03-29 19:37:14 -0300

    SellerService and basic FindAll

---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SellerService(SalesWebMVCContext context) //injeção  de dependencia
+        {
+            _context = context;
+        }
+        
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
