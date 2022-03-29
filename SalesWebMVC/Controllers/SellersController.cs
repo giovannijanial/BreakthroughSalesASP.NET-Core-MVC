f53122e53be83d8912506d5ce37506709d12fed2
Giovanni Janial <giovannijanial@gmail.com>
2022-03-29 20:29:31 -0300

    SellerFormViewModel and Department select component

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -5,15 +5,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMVC.Services;
 using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
+
 
 namespace SalesWebMVC.Controllers
 {
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService; //injeção de dependencia
-        public SellersController(SellerService sellerService)
+        private readonly DepartmentService _departmentService;//injeção de dependencia para o department service
+
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
         public IActionResult Index()
         {
@@ -23,7 +28,9 @@ namespace SalesWebMVC.Controllers
         
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments};
+            return View(viewModel);
         }
 
         [HttpPost]//anotation para um método post
---
+++ SalesWebMVC/Services/DepartmentService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMVC.Models;
+using SalesWebMVC.Data;
+
+
+namespace SalesWebMVC.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public DepartmentService(SalesWebMVCContext context) //injeção  de dependencia
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
